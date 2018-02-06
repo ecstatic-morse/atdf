eea1693174478a88e8dfc94f549b7cbb76aacdb7
Dylan MacKenzie <ecstaticmorse@gmail.com>
2018-02-06 12:17:46 -0800

    Remove minidom from tests

--- tests/parse.rs
+++ tests/parse.rs
@@ -1,5 +1,4 @@
 
-extern crate minidom;
 extern crate atdf;
 
 use std::fs::File;
@@ -10,11 +9,6 @@ fn parse(path: &str) {
     let file = File::open(path).unwrap();
     let file = BufReader::new(file);
     let atdf = atdf::parse(file).unwrap();
-
-    // let mut atdf = String::new();
-    // file.read_to_string(&mut atdf).unwrap();
-    // let root: minidom::Element = atdf.parse().unwrap();
-    // let devices = atdf::parse(&root).unwrap();
 
     let device = atdf.devices().first().unwrap();
     let d = atdf::device(device.clone(), atdf.modules());
